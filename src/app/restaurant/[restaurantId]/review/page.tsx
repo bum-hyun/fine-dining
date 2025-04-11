e40e998da6957933df7cbf346da3007721b2d4f4
bum-hyun <ru_bryunak@naver.com>
2025-04-11 15:09:06 +0900

    fix: 클라이언트 렌더링으로 바꿔봄

--- src/app/restaurant/[restaurantId]/review/page.tsx
+++ src/app/restaurant/[restaurantId]/review/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import RestaurantReviewList from '@/app/restaurant/[restaurantId]/review/RestaurantReviewList';
@@ -5,29 +7,29 @@ import { SERVICE_KEY } from '@/constants/service';
 import { getRestaurantNames } from '@/services/restaurant/restaurant_api';
 import { getRestaurantReviews } from '@/services/restaurant_review/restaurant_review_api';
 
-const Page = async (props: { params: Promise<{ restaurantId: string }> }) => {
-  const queryClient = new QueryClient();
-  const { restaurantId } = await props.params;
-
-  const params: IGetRestaurantReviewsParams = { restaurantId: Number(restaurantId), page: 0, limit: 10 };
-
-  await queryClient.prefetchQuery({
-    queryKey: [SERVICE_KEY.RESTAURANT.RESTAURANT_NAMES],
-    queryFn: getRestaurantNames,
-  });
-
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: [SERVICE_KEY.RESTAURANT_REVIEW.GET_RESTAURANT_REVIEWS, params],
-    queryFn: () => getRestaurantReviews(params),
-    initialPageParam: 0,
-  });
-
-  const dehydratedState = dehydrate(queryClient);
+const Page = (props: { params: Promise<{ restaurantId: string }> }) => {
+  // const queryClient = new QueryClient();
+  // const { restaurantId } = await props.params;
+  //
+  // const params: IGetRestaurantReviewsParams = { restaurantId: Number(restaurantId), page: 0, limit: 10 };
+  //
+  // await queryClient.prefetchQuery({
+  //   queryKey: [SERVICE_KEY.RESTAURANT.RESTAURANT_NAMES],
+  //   queryFn: getRestaurantNames,
+  // });
+  //
+  // await queryClient.prefetchInfiniteQuery({
+  //   queryKey: [SERVICE_KEY.RESTAURANT_REVIEW.GET_RESTAURANT_REVIEWS, params],
+  //   queryFn: () => getRestaurantReviews(params),
+  //   initialPageParam: 0,
+  // });
+  //
+  // const dehydratedState = dehydrate(queryClient);
 
   return (
-    <HydrationBoundary state={dehydratedState}>
-      <RestaurantReviewList />
-    </HydrationBoundary>
+    // <HydrationBoundary state={dehydratedState}>
+    <RestaurantReviewList />
+    // </HydrationBoundary>
   );
 };
 
